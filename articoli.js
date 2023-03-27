4c8d3178a25cb0dff0f5db80b41788d34ad26dd6
Andrea Lanciano <andrealanciano@me.com>
2023-03-27 14:57:43 +0200

    aggiunto card e filtro per nome

--- articoli.js
+++ articoli.js
@@ -1,9 +1,8 @@
 // fetch
 
 fetch('./articoli.json').then((response)=> response.json() ).then((data)=>{
-    
-    
 
+    // qui comincia script accordion
 
     let radioWrapper = document.querySelector('#radioWrapper');
     
@@ -13,18 +12,6 @@ fetch('./articoli.json').then((response)=> response.json() ).then((data)=>{
     let brands = data.map((articolo) => articolo.type);
     console.log(brands);
 
-    /* let uniqueBrands = [];
-
-    
-    brands.forEach((type) => {
-        
-        if(!uniqueBrands.includes(type)){
-            uniqueBrands.push(type);
-        }
-   });
-     
-
-   console.log(uniqueBrands); */
 
    let uniqueBrands = Array.from( new Set(brands));
 
@@ -39,4 +26,48 @@ fetch('./articoli.json').then((response)=> response.json() ).then((data)=>{
 
     setBrandRadios();
 
-    })
+
+
+    
+
+    //qui cominciano le card
+
+
+    let cardWrapper = document.querySelector('#cardWrapper');
+
+    //mostro le cards
+    function showCard(array) {
+        cardWrapper.innerHTML = '';
+
+            array.forEach((annuncio) => {
+                
+                let div = document.createElement('div');
+                div.classList.add('card', 'my-card');
+                div.innerHTML = 
+                `
+                <div class="card-body d-flex flex-wrap flex-column align-items-center justify-content-between">
+                <h5 class="card-title">${annuncio.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${annuncio.surname}</h6>
+                <img src="${annuncio.url}" class="card-img" alt="...">
+                </div>
+                `;
+                cardWrapper.appendChild(div);
+            });
+    }
+
+    showCard(data);
+
+
+    let wordInput = document.querySelector('#wordInput');
+
+    function filterByWord(parola){
+        let filtered = data.filter( (element) => element.name.toLowerCase().includes(parola.toLowerCase()) );
+        showCard(filtered);
+    }
+
+    wordInput.addEventListener('input', () => {
+        filterByWord(wordInput.value);
+    });
+
+
+});
